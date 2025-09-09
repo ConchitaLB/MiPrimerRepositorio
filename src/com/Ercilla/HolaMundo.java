7f76bd3c4af7ce26deb73ff86ac58a1563bd8bed
Conchitalb <mclb08@educastillalamancha.es>
2025-09-09 22:02:49 +0200

    Se añadió la clase Mensaje

--- src/com/Ercilla/HolaMundo.java
+++ src/com/Ercilla/HolaMundo.java
@@ -2,7 +2,8 @@ package com.Ercilla;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        Mensaje msg = new Mensaje();
+        String nombre = msg.SolicitarDatos("Introduce tu nombre: ");
+        msg.saludar(nombre);
     }
 }
