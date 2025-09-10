ce4ab4054c5186009c2ab0efe9d9c5a967be7944
Conchitalb <mclb08@educastillalamancha.es>
2025-09-10 18:21:11 +0200

    Se agregó una linea de código inservible

--- src/com/Ercilla/PersonaRegistro.java
+++ src/com/Ercilla/PersonaRegistro.java
@@ -2,6 +2,7 @@ package com.Ercilla;
 
 public record PersonaRegistro(String nombre, String apellidos) {
     public String toString(){
+        System.out.println("Esta insrucción es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 }
